0a143c862da4ce15176bcc4484584d1e0fbb3329
Jessica Burgos <32079816+jessBURGOS@users.noreply.github.com>
2018-07-09 18:41:07 -0700

    test final

--- map.js
+++ map.js
@@ -1,4 +1,22 @@
-var map = L.map("map", {
+var month_list = ["2010-12", "2011-03", "2011-06", "2011-09", "2011-12", "2012-03", "2012-06", "2012-09", "2012-12", "2013-03", "2013-06", "2013-09", "2013-12", "2014-03", "2014-06", "2014-09", "2014-12", "2015-03", "2015-06", "2015-09", "2015-12", "2016-03", "2016-06", "2016-09", "2016-12", "2017-03", "2017-06", "2017-09", "2017-12", "2018-03"];
+var populate_dropdown = d3.select("body").selectAll("#date-selector").selectAll("option").data(month_list).enter().append("option").text(function(d) {
+    return d;
+});
+
+var dd_value = d3.select("#date-selector").node().value; 
+
+console.log(dd_value);
+
+document.getElementById("date-selector").addEventListener("change", function() {
+    dd_value = d3.select("#date-selector").node().value;
+    console.log(dd_value);
+    drawMap();
+}, false);
+
+//Mapping piece
+function drawMap() {
+    console.log("running")
+    var map = L.map("map", {
     center: [37.752361, -122.080389],
     zoom: 9
   });
@@ -12,14 +30,10 @@ var map = L.map("map", {
       };
   var checkCounty = [];
   var combinedJSON;
-  d3.json("BayAreaCounties.geojson").then(function(data){
+  d3.json("./BayAreaCounties.geojson").then(function(data){
+    //   debugger;
     var newOb = data; 
-    // for (var i in newOb.features) {  
-    //     checkCounty.push(newOb.features[i].properties.county);   
-    //    }
-    //    for (var i in newOb) {  
-    //     myObject.jsonOne.push(newOb.features[i]);
-    //    }
+
         myObject.jsonOne.push(newOb)
         console.log(newOb)
     //    console.log(Object.keys(newOb.features[0].properties.county))
@@ -28,40 +42,34 @@ var map = L.map("map", {
   
   d3.json("https://cors-anywhere.herokuapp.com/" + "ucb-dbc-project2.herokuapp.com/county_single_family").then(function(data) {
     var anotherObject = data;
-    // console.log(JSON.stringify(anotherObject));
-    // const ordered = {};
-    // for (i in  anotherObject){
-    // Object.keys(anotherObject[i]).sort().forEach(function(key) {
-    // ordered[key] = unordered[key];
-    // })};
-    // for (i in  anotherObject){
-    //     data.sort(function(a, b) {
-    //         return a[i].RegionName > ba[i].RegionName;
-    //     });
-    // };
-    // data.sort();
-    // console.log(data);
-    // console.log(Object.keys(anotherObject[0]))
-    // for (var i in anotherObject){
-    //     console.log(anotherObject[i].RegionName)
-        
-    //     };
+    // debugger;
+    anotherObject = Object.values(anotherObject).sort((a, b) => {
+        var nameA = a.RegionName.toUpperCase();
+        var nameB = b.RegionName.toUpperCase();
+      
+        if (nameA < nameB) { return -1; }
+        if (nameA > nameB) { return 1; }
+        return 0;
+      });
+
     myObject.jsonTwo.push(anotherObject)
-    console.log(myObject.jsonTwo[0])
+    console.log(myObject.jsonTwo[0][0])
     console.log(myObject)
     
   // Grabbing data with d3...
     var geoJson;
+    console.log('APPLE')
     d3.map(myObject.jsonOne, function(data) {
+        console.log('BANANA')
         console.log(data)
         // console.log(data)
         // Creating a new choropleth layer
         geoJson = L.choropleth(data, {
         // Which property in the features to use
-        valueProperty: function (feature) {
-            for (i in myObject.jsonTwo[0]){
-            return myObject.jsonTwo[0][i]["1996-04"]
-            }
+        valueProperty: function (feature, i) {
+            var foo = myObject.jsonTwo[0].find(a => a.RegionName == feature.properties.county)[`${dd_value}`];
+            // debugger;
+            return foo;
           },
         // Color scale
         scale: ["#ffffb2", "#b10026"], 
@@ -128,4 +136,8 @@ var map = L.map("map", {
     legend.addTo(map);
   
   });
+  console.log('CHERRY')
 });
+}
+
+drawMap()
